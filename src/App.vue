cba9c872ab880d1161a3a537529cb75c6b8075b9
EhWanu <cameronjshadgett@gmail.com>
2022-02-04 16:35:05 -0500

    reorganized code so showing score is it's own component

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,16 @@
 		:delay="delay"
 		@end="endGame"
 	/>
-	<p v-if="showResults">
-		Reaction time: {{ score }}ms
-	</p>
+	<Results v-if="showResults" :score="score" />
 </template>
 
 <script>
 import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
 
 export default {
 	name: "App",
-	components: { Block },
+	components: { Block, Results },
 	data() {
 		return {
 			isPlaying: false,
