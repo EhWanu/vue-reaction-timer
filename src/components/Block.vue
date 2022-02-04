1411dd0bb495fd8d97cc868d5c16c88305f28a42
EhWanu <cameronjshadgett@gmail.com>
2022-02-04 16:21:05 -0500

    added timer for reaction speed and console logged the output

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,9 @@
 <template>
-	<div class="block" v-if="showBlock">
+	<div
+		class="block"
+		v-if="showBlock"
+		@click="stopTimer"
+	>
 		Click Me!
 	</div>
 </template>
@@ -10,20 +14,26 @@ export default {
 	data() {
 		return {
 			showBlock: false,
+			timer: null,
+			reactionTime: 0,
 		};
 	},
 	mounted() {
-		console.log("component mounted");
 		setTimeout(() => {
 			this.showBlock = true;
+			this.startTimer();
 		}, this.delay);
-		console.log(this.delay);
 	},
-	updated() {
-		console.log("component updated");
-	},
-	unmounted() {
-		console.log("unmounted");
+	methods: {
+		startTimer() {
+			this.timer = setInterval(() => {
+				this.reactionTime += 10;
+			}, 10);
+		},
+		stopTimer() {
+			clearInterval(this.timer);
+			console.log(this.reactionTime);
+		},
 	},
 };
 </script>
