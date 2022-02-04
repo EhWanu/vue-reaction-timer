98e4187ab992a9283d427996e5dccac4db6f0dc5
EhWanu <cameronjshadgett@gmail.com>
2022-02-04 16:30:09 -0500

    conditionally render results after the user has played/if the user is playing

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@
 	<button @click="start" :disabled="isPlaying">
 		Play
 	</button>
-	<Block v-if="isPlaying" :delay="delay" />
+	<Block
+		v-if="isPlaying"
+		:delay="delay"
+		@end="endGame"
+	/>
+	<p v-if="showResults">
+		Reaction time: {{ score }}ms
+	</p>
 </template>
 
 <script>
@@ -16,12 +23,20 @@ export default {
 		return {
 			isPlaying: false,
 			delay: null,
+			score: null,
+			showResults: false,
 		};
 	},
 	methods: {
 		start() {
 			this.delay = 2000 + Math.random() * 7000;
 			this.isPlaying = true;
+			this.showResults = false;
+		},
+		endGame(reactionTime) {
+			this.score = reactionTime;
+			this.isPlaying = false;
+			this.showResults = true;
 		},
 	},
 };
